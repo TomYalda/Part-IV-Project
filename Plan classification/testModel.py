a4b240b5b5ff6d30809cecc90e377081d91a2a70
Tom Yalda <tyal718@aucklanduni.ac.nz>
2025-07-31 10:09:17 +1200

    Reran the initial plan classifier and changes some of the path routes in the files

--- Plan classification/testModel.py
+++ Plan classification/testModel.py
@@ -1,12 +1,13 @@
 from ultralytics import YOLO
 import numpy as np
+import os
 
 # Load the best weights from the previous run
-model = YOLO("runs/classify/train2/weights/best.pt")
+model = YOLO("runs/classify/train4/weights/best.pt")
 
 # Predict on new data
 predictions = model.predict(
-    source=r'E:\data\plan_classification\test',
+    source=r'C:\Users\Administrator\Documents\data\plan_classification\test',
 )
 
 # Write results to a file
@@ -22,8 +23,9 @@ with open("prediction_results.txt", "w") as f:
         predicted_index = np.array(probs).argmax()
         predicted_name = model.names[predicted_index]
         confidence = probs[predicted_index]
+        image_name = os.path.basename(result.path)
 
-        print(f"Image: {result.path.split('\\')[-1]}   --------   Predicted class: {predicted_name} with probability {confidence:.2f}")
-        f.write(f"Image: {result.path.split('\\')[-1]}   --------   Predicted class: {predicted_name} with probability {confidence:.2f}\n")
+        print(f"Image: {image_name}   --------   Predicted class: {predicted_name} with probability {confidence:.2f}")
+        f.write(f"Image: {image_name}   --------   Predicted class: {predicted_name} with probability {confidence:.2f}\n")
 
 print("Prediction completed and results saved to 'prediction_results.txt'.")
--- Plan classification/trainModel.py
+++ Plan classification/trainModel.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
 # Build from YAML and load weights
-model = YOLO("yolov8n-cls.pt")
+model = YOLO("yolov8l-cls.pt")
 
 # Train the model
 print("Starting training...")
-results = model.train(data=r'E:\data\plan_classification\train', epochs=100, imgsz=64)
+results = model.train(data=r'C:\Users\Administrator\Documents\data\plan_classification\train', epochs=100, imgsz=600)
 print("Training completed.")
 
 # Save the model
