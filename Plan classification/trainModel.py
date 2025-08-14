d49c1ed5cf18fe59732e694aa09f09642ff75da4
Tom Yalda <tyal718@aucklanduni.ac.nz>
2025-08-14 21:27:59 +1200

    Implemented a third classifier with an image size of 1200

--- Plan classification/trainModel.py
+++ Plan classification/trainModel.py
@@ -1,11 +1,18 @@
+import torch
+import os
+
+torch.set_num_threads(16)
+os.environ["OMP_NUM_THREADS"] = "16"
+os.environ["MKL_NUM_THREADS"] = "16"
+
 from ultralytics import YOLO
 
 # Build from YAML and load weights
-model = YOLO("runs/classify/train5/weights/last.pt")
+model = YOLO("runs/classify/train8/weights/last.pt")
 
 # Train the model
 print("Starting training...")
-results = model.train(data=r'C:\Users\Administrator\Documents\data\plan_classification\train', epochs=100, imgsz=1200, patience=10)
+results = model.train(data=r'C:\Users\Administrator\Documents\data\plan_classification\train', epochs=100, imgsz=1200, patience=10, resume=True, workers=8)
 print("Training completed.")
 
 # Save the model
